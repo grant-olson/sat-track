d56979cab67400e44a964dba72322112e36bdd8b
Grant T. Olson <kgo@grant-olson.net>
2019-11-26 09:43:28 -0500

    Actually track with robot arm

--- track.py
+++ track.py
@@ -1,4 +1,5 @@
 from satellite_directory import Home, SatelliteDirectory
+from antenna_robot import AntennaRobot
 from time import sleep
 
 home = Home("EN90xj")
@@ -8,12 +9,12 @@ directory.add_keplerian("NOAA 18",
                     "1 28654U 05018A   19329.58873065  .00000093  00000-0  75226-4 0  9993",
                     "2 28654  99.0757  18.7938 0015197  88.8661 271.4251 14.12469460748097")
 
-az_el = directory.get_current_azimuth_and_elevation("NOAA 18", home)
+robot = AntennaRobot(17,18, elevation_max=2350)
 
 def decimal_angle(degrees,minutes,seconds):
     return float(degrees) + (float(minutes) / 60) + (float(seconds) / 3600)
 
-def track_it(satellite, home):
+def track_it(robot, satellite, home):
     started_pass = False
 
     while True:
@@ -21,11 +22,15 @@ def track_it(satellite, home):
         az_d, az_m, az_s = az.dms()
         decimal_az = decimal_angle(az_d, az_m, az_s)
 
+        if decimal_az < 0:
+            decimal_az += 360
+        
         el_d, el_m, el_s = el.dms()
         decimal_el = decimal_angle(el_d, el_m, el_s)
     
         if decimal_el < 0:
             print("BELOW HORIZON %d" % decimal_el)
+            robot.update(decimal_az, 0.0)
             if started_pass:
                 print("FINISHED TRACKING")
                 return
@@ -33,10 +38,11 @@ def track_it(satellite, home):
                 sleep(30)
 
         else:
-            print("TRACKING")
+            robot.update(decimal_az, decimal_el)
+            print("TRACKING %d %d" % (decimal_az, decimal_el))
             sleep(0.05)
             started_pass = True
         
 
 if __name__ == "__main__":
-    track_it("NOAA 18", home)
+    track_it(robot, "NOAA 18", home)
