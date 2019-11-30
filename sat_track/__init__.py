d5a9fe0f08bf88098c876b2f5b3fcde51b6556e4
Grant T. Olson <grant@webkite.com>
2019-11-30 13:39:13 -0500

    Use config for antenna robot instantiation

--- sat_track/__init__.py
+++ sat_track/__init__.py
@@ -2,3 +2,4 @@ from .antenna_robot import AntennaRobot
 from .servo import Servo
 from .satellite_directory import Home, SatelliteDirectory, todays_directory
 from .tracker import Tracker
+from .config import *
