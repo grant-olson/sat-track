3320485339f022d002b1399a48fc7e99de206c31
Grant T. Olson <grant@webkite.com>
2019-12-06 11:35:49 -0500

    Account for adjustments by user in robot

--- sat_track/antenna_robot.py
+++ sat_track/antenna_robot.py
@@ -9,6 +9,8 @@ class AntennaRobot:
         self.elevation_servo = Servo(elevation_pin, elevation_min, elevation_max)
         self.dir = "forward"
         self.update(0,0)
+        self.elevation_offset = 0.0
+        self.azimuth_offset = 0.0
         sleep(0.5)
 
     def update(self, azimuth, elevation):
@@ -18,8 +20,25 @@ class AntennaRobot:
         if azimuth < 0.0 or azimuth > 360.0:
             raise RuntimeError("Azimuth %s should be between 0-360" % azimuth)
 
-        if elevation < 0.0 or elevation > 360.0:
-            raise RuntimeError("Elevation %s should be between 0-360" % elevation)
+        if elevation < 0.0 or elevation > 180.0:
+            raise RuntimeError("Elevation %s should be between 0-180" % elevation)
+
+        # Add offsets and fix if we over/underflow
+        elevation += self.elevation_offset
+        azimuth += self.azimuth_offset
+
+        if elevation > 180.0:
+            elevation -= 180.0
+        elif elevation < 0.0:
+            elevation += 180.0
+
+        if azimuth > 360.0:
+            azimuth -= 360.0
+        elif azimuth < 0.0:
+            azimuth += 360
+
+        # Flip control since servos only go 180 degrees
+            
         if azimuth > 180.0:
             azimuth -= 180
             elevation = 180 - elevation
@@ -30,6 +49,7 @@ class AntennaRobot:
         self.azimuth_servo.set_angle(azimuth)
         self.elevation_servo.set_angle(elevation)
 
+        # pause a bit if we need to flip controls
         if current_dir != self.dir:
             self.dir = current_dir
             sleep(1)
